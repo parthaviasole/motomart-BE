4d3734fd53cbe6e47e28efafab498c3565371677
Parth <230377467+parth_nextere@users.noreply.github.com>
2026-03-25 13:16:57 +0530

    changes reverted

--- obj/Debug/net9.0/motomart-BE.AssemblyInfo.cs
+++ obj/Debug/net9.0/motomart-BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("motomart-BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634fe9ca14044ff17ceaab541cbd7a3f708f3691")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9d18a3190460890ba0f64ffa017061255ec01a")]
 [assembly: System.Reflection.AssemblyProductAttribute("motomart-BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("motomart-BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
